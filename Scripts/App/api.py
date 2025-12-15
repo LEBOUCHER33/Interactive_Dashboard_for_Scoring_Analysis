93d5c01a40655652a3ef79f63c72c93e92f4a5a4
Sophie LEBOUCHER <Sophie.LEBOUCHER@cea.fr>
2025-12-15 14:48:05 +0100

    feat/deploiement : modif des dockerfiles + dockerignore

--- Scripts/App/api.py
+++ Scripts/App/api.py
@@ -59,27 +59,9 @@ from fastapi.middleware.cors import CORSMiddleware
 # //////////////////////////////////////////////////
 
 
-# on héberge notre dataset sur le cloud
 
-#https://drive.google.com/file/d/1O8nJnYQnTolRfoP4mFyc13OlZBeAm-Nv/view?usp=drive_link
-
-FILE_ID="1O8nJnYQnTolRfoP4mFyc13OlZBeAm-Nv"
-
-url = f"https://drive.google.com/uc?export=download&id={FILE_ID}"
-
-
-# on définit une fonction pour loader uniquement un échantillon du dataset pour le déploiement sur le cloud
-
-def load_df_sample(n=500):
-    response = requests.get(url)
-    response.raise_for_status()
-    return pd.read_csv(
-        io.BytesIO(response.content),
-        sep=",",
-        engine="python",
-        nrows=n
-    )
-
+df =  pd.read_csv("./Data/Data_cleaned/data_sample.csv")
+df = df.replace({np.nan: None, np.inf: None, -np.inf: None})
 
 
 # //////////////////////////////////////////////////
@@ -114,16 +96,29 @@ GLOBAL_EXPLAINER = None
 async def lifespan(app: FastAPI):
     global CACHED_METRICS
     print("[STARTUP] Lancement de l'API...")
-    df_sample = load_df_sample(n=500)
-    df_sample = df_sample.replace({np.nan: None, np.inf: None, -np.inf: None})
-    raw_metrics = compute_metrics(
-        df=df_sample,
-        model_pipeline=model_pipeline,
-        explainer=None,
-        features_mapping=features_mapping,
-        sample_size=len(df_sample)
-    )
-    CACHED_METRICS=jsonable_encoder(raw_metrics)
+    # 1. Chargement/Calcul de l'Explainer
+    try:
+        print("[STARTUP] Calcul de l'explainer SHAP...")
+        GLOBAL_EXPLAINER = shap.TreeExplainer(model)
+        print("[STARTUP] Explainer prêt.")
+    except Exception as e:
+        print(f"[STARTUP] Erreur Explainer : {e}")
+    # 2. Tentative de calcul des métriques 
+    try:
+        print("[STARTUP] Tentative de pré-calcul des métriques...")
+        raw_metrics = compute_metrics(
+            df=df, 
+            model_pipeline=model_pipeline,
+            explainer=GLOBAL_EXPLAINER,
+            features_mapping=features_mapping,
+            sample_size=len(df)
+        )
+        CACHED_METRICS = jsonable_encoder(raw_metrics)
+        print("[STARTUP] Métriques pré-calculées avec succès !")
+    except Exception as e:
+        print(f"[STARTUP] Échec du pré-calcul : {e}")
+        CACHED_METRICS = None
+    
     yield
     print("[SHUTDOWN] Arrêt de l'API.")
 
---
+++ Scripts/App/dataset_initialization.py
@@ -0,0 +1,14 @@
+
+# pour déployer on va échantilloner le dataset
+
+import pandas as pd
+
+# 1- on load le dataset
+df =  pd.read_csv("./Data/Data_cleaned/application_test_final.csv")
+
+# 2-  on échantillonne
+df_sample = df.sample(n=500, random_state=42)
+
+# 3- on crée le csv
+df_sample.to_csv("Data/Data_cleaned/data_sample.csv", index=False)
+
